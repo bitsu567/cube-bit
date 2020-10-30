f82807d767c7f7c23206d6b1397953a98c0d1260
Hongsu Wu <hongsuwu@bitsu.local>
2020-10-30 09:39:23 +0800

    [feat] 添加button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Button from './components/Button/button';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <Button btnType='default' size='lg'>
+          default
+        </Button>
+        <Button
+          btnType='primary'
+          className='good'
+          onClick={(e) => console.log(333)}
+        >
+          primary
+        </Button>
+        <Button btnType='link' target='_blank' href='./'>
+          link
+        </Button>
+        <Button btnType='link' disabled target='_blank' href='./'>
+          link
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes, FC } from 'react';
+import classNames from 'classnames';
 
-export const Button = () => {
-  return <div>123123</div>;
+type ButtonSize = 'lg' | 'sm';
+
+type ButtonType = 'primary' | 'default' | 'danger' | 'link';
+
+interface BaseButtonProps {
+  btnType?: ButtonType;
+  size?: ButtonSize;
+  className?: string;
+  href?: string;
+}
+
+type ButtonProps = BaseButtonProps &
+  ButtonHTMLAttributes<HTMLElement> &
+  AnchorHTMLAttributes<HTMLElement>;
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  btnType,
+  size,
+  className,
+  disabled,
+  href,
+  ...restProps
+}) => {
+  const klass = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === 'link' && disabled,
+  });
+  if (btnType === 'link' && href) {
+    return (
+      <a className={klass} href={href} {...restProps}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={klass} disabled={disabled} {...restProps}>
+        {children}
+      </button>
+    );
+  }
 };
 
 export default Button;
